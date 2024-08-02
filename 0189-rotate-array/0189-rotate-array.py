337cdaf11009a9e42cb234a13a351dae8226d994
Rafli Ardiansyah <91625152+bulgogipedas@users.noreply.github.com>
2024-08-02 11:04:58 +0700

    Time: 164 ms (36.62%), Space: 27.9 MB (82.74%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,17 +4,8 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        k %= n  # Mengambil modulus untuk menangani k yang lebih besar dari n
-        
-        # Fungsi untuk membalikkan bagian dari array
-        def reverse(start: int, end: int) -> None:
-            while start < end:
-                nums[start], nums[end] = nums[end], nums[start]
-                start, end = start + 1, end - 1
-        
-        # Langkah 1: Balikkan seluruh array
-        reverse(0, n - 1)
-        # Langkah 2: Balikkan bagian pertama dari array
-        reverse(0, k - 1)
-        # Langkah 3: Balikkan bagian kedua dari array
-        reverse(k, n - 1)
+        k = k % n
+
+        rotate = nums[-k:] + nums[:-k]
+        for i in range(n):
+            nums[i] = rotate[i]
